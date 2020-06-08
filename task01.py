0282752233f6c310b882b69aa6d7e4ac0423eaa3
stikbomb <stikbomb@gmail.com>
2020-06-08 21:13:32 +0300

    task01 added

---
+++ task01.py
@@ -0,0 +1,16 @@
+# Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+# Об окончании ввода данных свидетельствует пустая строка.
+
+
+def write_to_file(filename, data):
+    with open(filename, 'a') as file:
+        file.write(data + '\n')
+
+
+if __name__ == '__main__':
+    while True:
+        input_data = f'{input("Добавить строку: ")}'
+        if input_data == '':
+            break
+        else:
+            write_to_file('task01.txt', input_data)
