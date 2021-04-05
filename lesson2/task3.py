cd1a2a5f0ff49ead1b3d952351df9e1c78b9ea4c
rsaleev <rs.aleev@gmail.com>
2021-04-05 21:31:36 +0300

    input check

--- lesson2/task3.py
+++ lesson2/task3.py
@@ -5,7 +5,7 @@ seasons = {'winter':[12,1,2],
             'summer':[6,7,8],
             'autumn':[9,10,11]}
 month = input('Введите номер месяца: ')
-if not month.isdigit():
-    print('Неверный ввод')
-else:
+if  month.isdigit() and 0< int(month)<=12:
     print(next(k for k,v in seasons.items() if int(month) in v))
+else:
+    print('Неверный ввод')
