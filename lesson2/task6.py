3fa24a69b9e24fdaa9a0de0677c6ff0e625bad6b
rsaleev <rs.aleev@gmail.com>
2021-04-08 21:39:31 +0300

    исправлен ввод

--- lesson2/task6.py
+++ lesson2/task6.py
@@ -1,27 +1,25 @@
-store = [
-    (1, {
-        'название': 'компьютер',
-        'цена': 20000,
-        'количество': 5,
-        'ед': 'шт'
-    }),
-    (2, {
-        'название': 'принтер',
-        'цена': 6000,
-        'количество': 2,
-        'eд': 'шт.'
-    }),
-    (3, {
-        'название': 'сканер',
-        'цена': 2000,
-        'количество': 7,
-        'eд': 'шт.'
-    }),
-]
 
-display={
-        'название':list(set([v for elem in store for k,v in elem[1].items() if k=='название'])),
-        'цена':list(set([v for elem in store for k,v in elem[1].items() if k=='цена'])),
-        'количество':list(set([v for elem in store for k,v in elem[1].items() if k=='количество'])),
-        'ед':list(set([v for elem in store for k,v in elem[1].items() if k=='ед']))}
-print(display)
+storage =[]
+
+while 1:
+    item = input('Введите название товара, цену товара, количество и единицу измерения через запятую или EOF для окончания ввода данных:')
+    if not item.upper() == 'EOF':
+        item_name, item_price, item_qty, item_unit = tuple(item.split(','))
+        if item_price.isalnum() and item_qty.isalnum():
+            idx = len(storage)+1
+            item_for_arr = (idx, {
+                'название': item_name,
+                'цена': int(item_price),
+                'количество': int(item_qty),
+                'ед': 'шт'
+                }) 
+            storage.append(item_for_arr)
+        else:
+            print('Введены неверные данные')
+    else:      
+        display={
+                'название':list(set([v for elem in storage for k,v in elem[1].items() if k=='название'])),
+                'цена':list(set([v for elem in storage for k,v in elem[1].items() if k=='цена'])),
+                'количество':list(set([v for elem in storage for k,v in elem[1].items() if k=='количество'])),
+                'ед':list(set([v for elem in storage for k,v in elem[1].items() if k=='ед']))}
+        print(display)
