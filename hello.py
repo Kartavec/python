ffa0a34ac6ad9c61463d748e15a12c40f4db865b
Maksim Krickij <maksim.krickij@gmail.com>
2021-04-29 19:29:31 +0300

    Solution for exercise5

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Привет мир")
+earnings = int(input("Enter the company earnings: "))
+expenses = int(input("Enter the company expenses: "))
+if earnings < expenses:
+    print("Your company is non profitable ")
+
+else:
+    profit = earnings - expenses
+    print(f"Your company's profit is {profit}")
