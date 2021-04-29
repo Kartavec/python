a584ef89b7cf2e698d593fe052383238292cc95f
Maksim Krickij <maksim.krickij@gmail.com>
2021-04-29 19:02:55 +0300

    exercise 1 solution

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Привет мир")
+a  = 1
+b = 2.111
+print(f"a = {a},b={b}")
+print(float(a + b),"= a+b")
+user = str(input("Enter your username: "))
+password = int(input("Enter your password: "))
+print(f"Your name {user}, Your password {password}")
