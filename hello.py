c3ee63ceaecb945739a4a6713d3272ef6feb0f9a
Maksim Krickij <maksim.krickij@gmail.com>
2021-04-29 19:11:11 +0300

    solution for exercise_2

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Привет мир")
+time = int(input("Type down your time in seconds:  "))
+hours = time // 3600
+minutes = time % 3600 // 60
+seconds = time % 60
+print(f"time = {hours:02}:{minutes:02}:{seconds:02}")
