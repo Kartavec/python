a84f77cd311a85dbd1077bb8ce558efc1f850b25
Maksim Krickij <maksim.krickij@gmail.com>
2021-04-29 20:00:31 +0300

    Solution for exercise_6

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Привет мир")
+a = int(input("Enter the first day running distance: "))
+b = int(input("Enter the distance you want to achieve: "))
+c = 1
+while a < b:
+    a = a * 1.1
+    c = c + 1
+print(c)
