21aa6c173d72f7b979c4335bec01fd89b632e654
Maksim Krickij <maksim.krickij@gmail.com>
2021-04-29 19:54:11 +0300

    Changes for exercise_5 solution

--- hello.py
+++ hello.py
@@ -6,3 +6,6 @@ if earnings < expenses:
 else:
     profit = earnings - expenses
     print(f"Your company's profit is {profit}")
+e = int(input("Write the number of employees on your company"))
+e = e / profit
+print(f"Pforit for 1 employee {e}")
