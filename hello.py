920946863598145e56371f6f9c342f6fb570cf4d
Maksim Krickij <maksim.krickij@gmail.com>
2021-04-29 19:24:25 +0300

    Solution for exrcise_4

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Привет мир")
+num = int(input("Type your positive integer number: "))
+n = num
+max = n % 10
+while n:
+    i = n % 10
+    if i > max:
+        max = i
+    n = n // 10
+print(f'The greatest figure in the number is {max} ')
