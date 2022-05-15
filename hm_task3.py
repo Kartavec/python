138ab512a25e414afcef6f668adc8c2256c89f43
netdaem0n <bakalets@gmail.com>
2022-05-15 11:33:08 +1100

    -

--- hm_task3.py
+++ hm_task3.py
@@ -1,12 +1,15 @@
 def check_var1(word):
     try:
         bytes(word, encoding='ascii')
-    except:
+    except UnicodeEncodeError:
         print(word, "cannot be transformed to bytes")
+
+
 def check_var2(word):
     array_errors = [x for x in word if ord(x) > 127]
     if len(array_errors) > 0:
         print(word, "cannot be transformed to bytes")
+
 
 words_bank = ["attribute", "класс", "функция", "type"]
 print("var 1:")
