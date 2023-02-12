31e376863947bfb559892176e51c370ece42abe5
Евгений Павлович Погорелый <99702662+evgeniypogoreliy@users.noreply.github.com>
2023-02-13 10:12:19 +1100

    Practical work 8

---
+++ Less_5.py
@@ -0,0 +1,41 @@
+# 5. Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники
+# на склад и передачу в определенное подразделение компании. Для хранения данных
+# о наименовании и количестве единиц оргтехники, а также других данных, можно использовать
+# любую подходящую структуру (например словарь).
+
+class Sklad:
+    def __init__(self):
+        self._dict = {}
+
+    def add_to(self, equipment):
+        self._dict.setdefault(equipment.group_name(), []).append(equipment)
+
+    def extract(self, name):
+        if self._dict[name]:
+            self._dict.setdefault(name).pop(0)
+
+
+class Equipment:
+    def __init__(self, name, make, year):
+        self.name = name
+        self.make = make
+        self.year = year
+        self.group = self.__class__.__name__
+
+    def group_name(self):
+        return f'{self.group}'
+
+    def __repr__(self):
+        return f'{self.name} {self.make} {self.year}'
+
+
+class Printer(Equipment):
+    def __init__(self, series, name, make, year):
+        super().__init__(name, make, year)
+        self.series = series
+
+    def __repr__(self):
+        return f'{self.name} {self.series} {self.make} {self.year}'
+
+    def action(self):
+        return 'Печатает'
