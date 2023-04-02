25fcf5adfa6ce551582f4c63ad67abc9e35e0d76
kramastic <kramastic@gmail.com>
2023-04-02 20:18:28 +0300

    вв

--- lesson4/task3.py
+++ lesson4/task3.py
@@ -1,7 +1,7 @@
-#решение
-generator = (numb for numb in range(20, 241) if (numb % 20 == 0) or (numb %  21 == 0) )
+# решение
+generator = (numb for numb in range(20, 241) if (numb % 20 == 0) or (numb % 21 == 0))
 
-#вывод решения
+# вывод решения
 for i in generator:
     print(i)
 
